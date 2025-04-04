ae66a77b12c21ed8ec0df3c131d3e11cc9447072
mohamed lakkis <35637294+mohalakkis@users.noreply.github.com>
2025-04-04 11:05:38 -0300

    Update Main.java
    update lambda

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         CalcSomar somaLambda = (a, b) -> a + b;
 
+        int result1 = somaLambda(1,2);
+
         int result = somaLambda.somar(3,9);
         System.out.println(result);
 
